7855137a9450011ad2ed48e0667e23cdddc604a5
Gustavo M. Benitez <gustavo.benitez@biting.com.ar>
2022-01-25 12:53:56 -0300

    inicializacion-componentes

---
+++ src/screens/product-detail/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+import styles from './styles';
+
+const ProductDetail = ({navigation, route}) => {
+  const {product} = route.params;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>{product.name}</Text>
+        <Text>{product.description}</Text>
+        <Text>{product.weight}</Text>
+        <Text>$ {product.price}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ProductDetail;
