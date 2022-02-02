8c7364198e12a5aac5d9aa5d59a51673bd3b8830
Gustavo M. Benitez <gustavo.benitez@biting.com.ar>
2022-02-02 10:49:19 -0300

    implementación redux

--- src/screens/products/index.js
+++ src/screens/products/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, FlatList, View} from 'react-native';
-import {BREADS} from 'utils/data/breads';
+import {useDispatch, useSelector} from 'react-redux';
+import {filterBreads, selectBread} from '../../store/actions/breads.action';
 import ProductItem from '../../components/products-item/index';
 import styles from './styles';
 
 const Products = ({navigation, route}) => {
-  const breads = BREADS.filter(
-    bread => bread.category === route.params.categoryId,
-  );
+  const dispatch = useDispatch();
+  const categoryBreads = useSelector(state => state.breads.filteredBread);
+  const category = useSelector(state => state.categories.selected);
 
   const handleSelectedProduct = item => {
+    dispatch(selectBread(item.id));
     navigation.navigate('ProductDetail', {
       productId: item.id,
       name: item.name,
@@ -20,11 +22,17 @@ const Products = ({navigation, route}) => {
   const renderProducts = ({item}) => {
     return <ProductItem item={item} onSelected={handleSelectedProduct} />;
   };
+
+  useEffect(() => {
+    dispatch(filterBreads(category.id));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <FlatList
-          data={breads}
+          data={categoryBreads}
           renderItem={renderProducts}
           keyExtractor={item => item.id}
         />
